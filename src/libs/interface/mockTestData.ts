649d611042e360e9162f39f5e21531c54d855292
Miguel-Angel-Guate <miguelangelteletor@yahoo.es>
2024-04-28 15:13:42 +0200

    update name data

--- src/libs/interface/mockTestData.ts
+++ src/libs/interface/mockTestData.ts
@@ -5,17 +5,17 @@ export const mockData: VerticalChartData = {
     labels: ["TOTAL", "36", "38", "40", "42", "44"],
     datasets: [
         {
-            label: "Venta (%)",
+            label: "January (%)",
             data: [65, 59, 80, 81, 56, 55, 40],
             backgroundColor: "#0D2E1B"
         },
         {
-            label: "Compra (%)",
+            label: "March (%)",
             data: [28, 48, 40, 19, 86, 27, 90],
             backgroundColor: "#2C995A"
         },
         {
-            label: "Eficacia (%)",
+            label: "April (%)",
             data: [25, 20, 60, 95, 56, 55, 40],
             backgroundColor: "#ABD6BD"
         }
